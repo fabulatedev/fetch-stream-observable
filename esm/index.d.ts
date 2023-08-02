3738c78f67ee05d8657265194aef31fb56d7414b
Ashish Shubham <ashubham@gmail.com>
2023-08-02 01:03:31 -0700

    remove build artifacts

--- esm/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from "zen-observable-ts";
-export interface FetchObservableOpts extends RequestInit {
-    openWhenHidden?: boolean;
-    fetch?: typeof fetch;
-    headers?: Record<string, string>;
-    parseOpenAIStream?: boolean;
-    additiveTextStream?: boolean;
-}
-export declare function fetchObservable(url: string, options?: FetchObservableOpts): Observable<string>;
